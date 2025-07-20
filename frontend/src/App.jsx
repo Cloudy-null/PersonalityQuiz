4ec79c2578129d89f7fcdb0dc208e9d00827bab1
Cloudy.null <Abdulrahman.Aljahdali0@gmail.com>
2025-07-20 09:45:16 +0300

    made the Q box, need to do 'next' button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Quiz from './components/Quiz'
+import Navbar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Quiz />
+    </div>
+  )
+}
+
+export default App
